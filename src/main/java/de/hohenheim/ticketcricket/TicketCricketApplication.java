50e992cb7e0e8c6de6d98c8ef307cf26b6d1a162
Levin <st166200@stud.uni-stuttgart.de>
2022-05-05 16:38:44 +0200

    Initial commit

---
+++ src/main/java/de/hohenheim/ticketcricket/TicketCricketApplication.java
@@ -0,0 +1,13 @@
+package de.hohenheim.ticketcricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketCricketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hohenheim/ticketcricket/TicketCricketApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hohenheim.ticketcricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCricketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
