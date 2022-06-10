e619b83b7b0ea9b64c4050530c1aa6ce5c2a06d4
Fabian <maischfa@gmail.com>
2022-06-10 11:03:14 +0200

    Did some changes to the Login-page

--- src/main/java/de/hohenheim/ticketcricket/controller/HomeController.java
+++ src/main/java/de/hohenheim/ticketcricket/controller/HomeController.java
@@ -3,6 +3,8 @@ package de.hohenheim.ticketcricket.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -17,6 +19,20 @@ public class HomeController {
     public String showHome(Model model) {
         model.addAttribute("message", "Und hier sehen Sie ein ModelAttribut");
         return "home";
+    }
+
+   @PostMapping("/user")
+    public String user(Model model, @RequestParam String uname, @RequestParam String password){
+        if(uname.equals("user") && password.equals("user")){
+            return "userTicketerstellung";
+        }
+        model.addAttribute("errorMsg", "Please provide the correct Username and Password");
+        return "login";
+    }
+
+    @PostMapping("/admin")
+    public String admin(){
+        return "userTicketerstellung";
     }
 
 }
