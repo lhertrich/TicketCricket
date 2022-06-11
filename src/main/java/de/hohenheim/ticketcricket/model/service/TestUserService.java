6170fe79eec69ae2f488dcf37f4c645a3b29034d
Sidney Strasser <sidstrasser@gmail.com>
2022-06-11 19:34:07 +0200

    testing some stuff

--- src/main/java/de/hohenheim/ticketcricket/model/service/TestUserService.java
+++ src/main/java/de/hohenheim/ticketcricket/model/service/TestUserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TestUserService {
@@ -20,4 +21,6 @@ public class TestUserService {
     public List<TestUser> findAllUsers() {
         return testUserRepository.findAll();
     }
+
+    public TestUser getUser(int id) { return testUserRepository.getById(id);}
 }
