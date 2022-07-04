9f6898ecf63dfff78bedbb7eee066b7236d0c924
Levin <st166200@stud.uni-stuttgart.de>
2022-07-04 22:36:27 +0200

    Fixed Bugs from NotBell and Search

--- src/main/resources/static/js/NotificationBell.js
+++ src/main/resources/static/js/NotificationBell.js
@@ -1,10 +1,11 @@
-$(function () {
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content"); $(document).ajaxSend(function (e, xhr, options) {
-        xhr.setRequestHeader(header, token); });
-});
-
 $(document).ready(function(){
+
+    $(function () {
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content"); $(document).ajaxSend(function (e, xhr, options) {
+            xhr.setRequestHeader(header, token); });
+    });
+
     $("#notificationUser").click(function (){
         $("#notificationUser").load("/ajax/set-notificationsRead");
     });
--- src/main/resources/static/js/SearchBar.js
+++ src/main/resources/static/js/SearchBar.js
@@ -1,11 +1,11 @@
-$(function () {
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content");
-    $(document).ajaxSend(function (e, xhr, options) {
-        xhr.setRequestHeader(header, token); });
-});
-
 $(document).ready(function(){
+    $(function () {
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
+        $(document).ajaxSend(function (e, xhr, options) {
+            xhr.setRequestHeader(header, token); });
+    });
+
     var searchString;
     $("#searchBar").keyup(function (){
         searchString = $("#searchBar").val().replace(/\s+/g, '-');
