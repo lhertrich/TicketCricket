a8ecaec5a154029a8392495b3dfc1edc8e541137
Levin <st166200@stud.uni-stuttgart.de>
2022-07-07 15:31:42 +0200

    Began sort function

---
+++ src/main/resources/static/js/Sort.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#sortSubmitButton").click(function (){
+        let category = $("#sortAttribute").val();
+        console.log("Kategorie: "+category);
+    });
+});
